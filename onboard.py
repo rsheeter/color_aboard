b09d165b264c97517efdf6d4364868bdcb885eb4
Rod S <rsheeter@google.com>
2022-04-10 21:09:12 -0700

    Tweaks to make helper more helpful

--- onboard.py
+++ onboard.py
@@ -5,6 +5,7 @@
 from absl import app
 from absl import flags
 from absl import logging
+from fontTools import ttLib
 from pathlib import Path
 import requests
 import shutil
@@ -14,42 +15,42 @@ from urllib.parse import urlparse
 import zipfile
 
 
+class FontFile(NamedTuple):
+    source: Path
+    dest: Path
+
+
 class FontSource(NamedTuple):
     url: str
-    fonts: Tuple[Path, ...]
-    family_dir: str
+    fonts: Tuple[FontFile, ...]
     git_dir: Optional[str] = None
     maximum_color_args: Tuple[str, ...] = ()
 
 _FONT_SOURCES = (
     FontSource(
         "https://github.com/aliftype/amiri/releases/download/0.114/Amiri-0.114.zip",
-        (Path("Amiri-0.114/AmiriQuranColored.ttf"),),
-        "ofl/amiriqurancolored",
+        (FontFile(Path("Amiri-0.114/AmiriQuranColored.ttf"), Path("ofl/amiriqurancolored/AmiriQuranColored-Regular.ttf")),),
         maximum_color_args=("--bitmap_resolution", "48")  # uh oh
     ),
     FontSource(
         "https://github.com/aliftype/aref-ruqaa/releases/download/v1.004/ArefRuqaa-1.004.zip",
         (
-            Path("ArefRuqaa-1.004/ttf/ArefRuqaaInk-Regular.ttf"),
-            Path("ArefRuqaa-1.004/ttf/ArefRuqaaInk-Bold.ttf"),
+            FontFile(Path("ArefRuqaa-1.004/ttf/ArefRuqaaInk-Regular.ttf"), Path("ofl/arefruqaaink/ArefRuqaaInk-Regular.ttf")),
+            FontFile(Path("ArefRuqaa-1.004/ttf/ArefRuqaaInk-Bold.ttf"), Path("ofl/arefruqaaink/ArefRuqaaInk-Bold.ttf")),
         ),
-        "ofl/arefruqaaink",
     ),
     FontSource(
         "https://github.com/aliftype/reem-kufi/raw/main/ReemKufiFun.ttf",
         (
-            Path("ReemKufiFun.ttf"),
+            FontFile(Path("ReemKufiFun.ttf"), Path("ofl/reemkufifun/ReemKufiFun[wght].ttf")),
         ),
-        "ofl/reemkufifun",
     ),
     # This one is private :(
     FontSource(
         "git@github.com:Gue3bara/Blaka.git",
         (
-            Path("fonts/BlakaInk-Regular.ttf"),
+            FontFile(Path("fonts/BlakaInk-Regular.ttf"), Path("ofl/blakaink/BlakaInk-Regular.ttf")),
         ),
-        "ofl/blakaink",
         git_dir="blaka_ink",
     ),
 )
@@ -96,16 +97,21 @@ def _fetch_fonts(font_sources):
         for font in font_source.fonts:
             if dest.suffix == '.zip':
                 with zipfile.ZipFile(dest, 'r') as zip_file:
-                    with open(_TEMP_DIR / font.name, 'wb') as f:
-                        f.write(zip_file.read(str(font)))
+                    with open(_TEMP_DIR / font.dest.name, 'wb') as f:
+                        f.write(zip_file.read(str(font.source)))
             elif font_source.url.startswith("git@"):
                 for font in font_source.fonts:
-                    source_file = _TEMP_DIR / font_source.git_dir / font
+                    source_file = _TEMP_DIR / font_source.git_dir / font.source
                     assert source_file.is_file(), f"Missing {source_file}"
-                    shutil.copyfile(source_file, _TEMP_DIR / source_file.name)
+                    shutil.copyfile(source_file, _TEMP_DIR / font.dest.name)
             else:
-                assert (_TEMP_DIR / font).is_file()
+                assert (_TEMP_DIR / font.source).is_file()
+                shutil.copyfile(_TEMP_DIR / font.source, _TEMP_DIR / font.dest.name)
 
+
+def _coloring(font_file):
+    font = ttLib.TTFont(font_file)
+    return [t for t in ["COLR", "SVG ", "sbix", "CBDT"] if t in font]
 
 
 def main(_):
@@ -113,23 +119,34 @@ def main(_):
 
     _fetch_fonts(_FONT_SOURCES)
 
+    print("Working in", _TEMP_DIR)
+    print("Final dests in", _GOOGLE_FONTS_DIR)
+
     fonts = _TEMP_DIR.glob("*.[ot]tf")
     for font_source in _FONT_SOURCES:
         for font in font_source.fonts:
-            font = _TEMP_DIR / font.name
-            assert font.is_file, f"Missing {font}"
-            conversion_dir = _TEMP_DIR / "maximum_color" / font.stem
+            font_file = _TEMP_DIR / font.dest.name
+            assert font_file.is_file, f"Missing {font_file}"
+            conversion_dir = _TEMP_DIR / "maximum_color" / font_file.stem
             conversion_dir.mkdir(parents=True, exist_ok=True)
-            cmd = ("maximum_color", font) + font_source.maximum_color_args
-            print("Maximizing", font, "in", conversion_dir, " ".join(str(c) for c in cmd))
-            subprocess.run(
-                cmd,
-                cwd=conversion_dir,
-                check=True,
-            )
-            converted_font = conversion_dir / "build" / font.name
-            print("Maximum version", converted_font)
-            print()
+            converted_font = conversion_dir / "build" / font_file.name
+
+            if not converted_font.is_file():
+                cmd = ("maximum_color", font_file, "--bitmaps", "--output_file", str(converted_font)) + font_source.maximum_color_args
+                print()
+                print("Maximizing", font, "in", conversion_dir, " ".join(str(c) for c in cmd))
+                subprocess.run(
+                    cmd,
+                    cwd=conversion_dir,
+                    check=True,
+                )
+
+            assert converted_font.is_file(), converted_font
+            final_dest = _GOOGLE_FONTS_DIR / font.dest
+            print(f"Working", converted_font.relative_to(_TEMP_DIR), ",".join(t.strip() for t in _coloring(converted_font)))
+            print("  ", final_dest.relative_to(_GOOGLE_FONTS_DIR))
+            final_dest.parent.mkdir(parents=True, exist_ok=True)
+            shutil.copyfile(converted_font, final_dest)
 
 
 if __name__ == "__main__":
